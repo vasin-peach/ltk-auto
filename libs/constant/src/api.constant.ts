8219c4582bfc5d287617585e04109c7634086ee8
Vasin Sermsampan <dev.vasin@gmail.com>
2022-11-15 18:23:43 +0700

    ci(nginx): update nginx

---
+++ libs/constant/src/api.constant.ts
@@ -0,0 +1,17 @@
+export enum ApiPaginationEnum {
+  pagination = "PAGINATION",
+}
+
+export enum ApiStatusEnum {
+  success = "SUCCESS",
+  notfound = "NOTFOUND",
+  error = "ERROR",
+}
+
+export enum ApiUsersEnum {
+  create = "CREATE_USER",
+  getUsers = "GET_USERS",
+  getUser = "GET_USER",
+  patchUser = "PATCH_USER",
+  remove = "REMOVE_USER",
+}
