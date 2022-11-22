82a0b5ad071da1db81cb2734310198944b74472b
Vasin Sermsampan <dev.vasin@gmail.com>
2022-11-22 11:16:38 +0700

    fix(*): update app name

--- apps/api/src/config/swagger/index.ts
+++ apps/api/src/config/swagger/index.ts
@@ -4,8 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export default function initSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .addBearerAuth()
-    .setTitle('LTKAuto')
-    .setDescription('LTKAuto API Documentation.')
+    .setTitle('My web')
+    .setDescription('My web API Documentation.')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
