b8ef08ba0ffc43b807237f70f43b9d5a96e2c006
Vasin Sermsampan <dev.vasin@gmail.com>
2022-12-20 23:27:45 +0700

    fix(eslint): fix tailwind not found

--- config/eslint-config-next/index.js
+++ config/eslint-config-next/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['turbo', 'prettier', 'tailwindcss', 'next/core-web-vitals'],
+  extends: ['turbo', 'prettier', 'plugin:tailwindcss/recommended', 'next/core-web-vitals'],
   ignorePatterns: ['node_modules', 'dist'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
