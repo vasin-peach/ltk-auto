fca0538d21fa0d3d807e2d5cef63ecd7be9b76b8
Vasin Sermsampan <dev.vasin@gmail.com>
2022-12-20 23:21:17 +0700

    feat(*): update

--- config/eslint-config-next/index.js
+++ config/eslint-config-next/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['turbo', 'prettier', 'next/core-web-vitals'],
+  extends: ['turbo', 'prettier', 'tailwindcss', 'next/core-web-vitals'],
   ignorePatterns: ['node_modules', 'dist'],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
     'turbo/no-undeclared-env-vars': 'off',
   },
-};
+}
